5edbeb2610be8451522e5a673d2de92bf75b8025
Evan Slawski <evan.slawski@ithaka.org>
2023-07-26 21:10:08 -0400

    Remove unused code from basic example

--- basic-example/mfe-navigation/webpack.config.js
+++ basic-example/mfe-navigation/webpack.config.js
@@ -1,23 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const DashboardPlugin = require('@module-federation/dashboard-plugin');
 const packageJson = require("./package.json");
 const TerserPlugin = require("terser-webpack-plugin");
-
-// const { readFileSync } = require('fs');
-// const tokens = readFileSync(__dirname + '/../.env')
-//     .toString('utf-8')
-//     .split('\n')
-//     .map(v => v.trim().split('='));
-// process.env.DASHBOARD_READ_TOKEN = tokens.find(([k]) => k === 'DASHBOARD_READ_TOKEN')[1];
-// process.env.DASHBOARD_WRITE_TOKEN = tokens.find(([k]) => k === 'DASHBOARD_WRITE_TOKEN')[1];
-// process.env.DASHBOARD_BASE_URL = tokens.find(([k]) => k === 'DASHBOARD_BASE_URL')[1];
 
 const {
     container: { ModuleFederationPlugin },
 } = require('webpack');
 const path = require('path');
-
-// const dashboardURL = `${process.env.DASHBOARD_BASE_URL}/env/development/get-remote?token=${process.env.DASHBOARD_READ_TOKEN}`;
 
 module.exports = {
     entry: './src/index',
@@ -81,57 +69,10 @@ module.exports = {
                 },
             },
         }),
-        // new ModuleFederationPlugin({
-        //     name: 'home',
-        //     filename: 'remoteEntry.js',
-        //     library: { type: 'var', name: 'home' },
-        //     remotes: {
-        //         dsl: DashboardPlugin.clientVersion({
-        //             currentHost: 'home',
-        //             remoteName: 'dsl',
-        //             dashboardURL,
-        //         }),
-        //         search: DashboardPlugin.clientVersion({
-        //             currentHost: 'home',
-        //             remoteName: 'search',
-        //             dashboardURL,
-        //         }),
-        //         nav: DashboardPlugin.clientVersion({
-        //             currentHost: 'home',
-        //             remoteName: 'nav',
-        //             dashboardURL,
-        //         }),
-        //         utils: DashboardPlugin.clientVersion({
-        //             currentHost: 'home',
-        //             remoteName: 'utils',
-        //             dashboardURL,
-        //         }),
-        //     },
-        //     // sharing code based on the installed version, to allow for multiple vendors with different versions
-        //     shared: {
-        //         ...deps,
-        //         "react": { singleton: true },
-        //         "react-dom": { singleton: true }
-        //     },
-        // }),
         new HtmlWebpackPlugin({
             template: './public/index.html',
             excludeChunks: ['remoteEntry'],
         }),
-        // new DashboardPlugin({
-        //     versionStrategy: `${Date.now()}`,
-        //     filename: 'dashboard.json',
-        //     dashboardURL: `${process.env.DASHBOARD_BASE_URL}/update?token=${process.env.DASHBOARD_WRITE_TOKEN}`,
-        //     versionChangeWebhook: 'http://cnn.com/',
-        //     metadata: {
-        //         clientUrl: process.env.DASHBOARD_BASE_URL,
-        //         baseUrl: 'http://localhost:3001',
-        //         source: {
-        //             url: 'https://github.com/module-federation/federation-dashboard/tree/master/dashboard-example/home',
-        //         },
-        //         remote: 'http://localhost:3001/remoteEntry.js',
-        //     },
-        // }),
     ],
     optimization: {
         chunkIds: "named",
